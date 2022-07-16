00c6306f1493c7f521565aff3b8d7c5cb11250c7
Oluwasegun Iyanda <ojiyanda@gmail.com>
2022-07-15 17:44:18 -0700

    new update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
 
 /**
-* _memset - Fills/overwrites memory at selected spot
-* @s: Pointer to begin writting to memory
-* @b: Byte to fill memory with
-* @n: Number of Bytes to write
-*
-* Return: Pointer to s
-*/
-
-char *_memset(char *s, char b, unsigned int n)
+ * _strcat - a function that concatenates two strings.
+ * @dest: an input string
+ * @src: an input string
+ * Return: A pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
 {
-	unsigned int i;
+	char *temp = dest;
 
-	for (i = 0; i < n; i++)
-		*(s + i) = b;
+	while (*dest)
+		dest++;
 
-	return (s);
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
 }
